ed2bf692b26acffbb3283e5ef1337bfc79c65ed9
Vaibhav10sept <vaibhavtiwari10sept@gmail.com>
2022-02-24 16:45:59 +0530

    intial commit

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// add this file to .gitignore
+
+module.exports = {
+      
+        mongodb: {
+            dbURI: 'mongodb+srv://vaibhav:vaibhav@cluster0.c5tnw.mongodb.net/collegeFinder?retryWrites=true&w=majority'
+        },
+       
+};
