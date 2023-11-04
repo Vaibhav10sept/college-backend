ee5880c79f3c22de2ea8d3ea22fcc60872b112d1
vaibhav-tiwariacc <vaibhav.tiwari@accolitedigital.com>
2023-11-04 18:14:22 +0530

    update index.js

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ const PORT = process.env.PORT || 8000
 const app = express();
 // app.use(bodyParser.json({ extended: false }));
 app.use(express.json())
-app.use(cors({origin: true,credentials:true}),bodyParser.urlencoded({extended: true}))
+app.use(cors({ origin: true, credentials: true }), bodyParser.urlencoded({ extended: true }))
 app.enable("trust proxy");
+
 
 // connect to mongodb
 // mongoose.connect(keys.mongodb.dbURI,{ useUnifiedTopology: 
@@ -23,36 +24,36 @@ app.enable("trust proxy");
 // });
 
 mongoose.connect(keys.mongodb.dbURI)
-  .then(function (db) { // <- db as first argument
-    console.log('connected to mongodb')
-  })
-  .catch(function (err) {})
+    .then(function(db) { // <- db as first argument
+        console.log('connected to mongodb')
+    })
+    .catch(function(err) {})
 
 const connection = mongoose.connection;
 
 connection.once("open", function() {
-console.log("MongoDB database connection established successfully");
+    console.log("MongoDB database connection established successfully");
 });
 
 
 app.use('/api/college', collegeRoutes);
-app.use('/api/student',studentRoutes);
+app.use('/api/student', studentRoutes);
 
 
 
 
 app.get('/', (req, res) => {
-  // const name = req.query.name || 'World';
-  // res.setHeader('Content-Type', 'application/json');
-  res.send("<H2>Hello, welcome to college finder backend.</h2>");
+    // const name = req.query.name || 'World';
+    // res.setHeader('Content-Type', 'application/json');
+    res.send("<H2>Hello, welcome to college finder backend.</h2>");
 });
 
 app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+    const name = req.query.name || 'World';
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.listen(process.env.PORT, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
